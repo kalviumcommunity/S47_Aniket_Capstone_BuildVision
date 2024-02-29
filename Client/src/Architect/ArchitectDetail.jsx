fcf26580fe7e1c55795a8fe02afbbfbfd1c3fd0b
aniketgoyal51 <aniketgoyal791@gmail.com>
2024-02-29 12:26:36 +0530

    Created the Client detail page in front-end

--- Client/src/Architect/ArchitectDetail.jsx
+++ Client/src/Architect/ArchitectDetail.jsx
@@ -9,36 +9,36 @@ function ArchitectDetail() {
         console.log(data)
     }
     return (
-        <div className={form.page}>
+        <div className={form.Archipage}>
             <img src={formimage} alt="" className={form.image} />
-            <div className={form.box}>
+            <div className={form.Archibox}>
                 <h1>Architect</h1>
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <div className={form.detail}>
                         <label>Name</label>
                         <input type='text' {...register("ArchitectName", { required: "Architect Name is required", pattern: { value: /^[a-zA-Z]+$/, message: "Only Alphabets are allowed" } })} placeholder="Enter Architect Name" />
-                        {errors.ArchitectName && <p>{errors.ArchitectName.message}</p>}
                     </div>
+                        {errors.ArchitectName && <p>{errors.ArchitectName.message}</p>}
                     <div className={form.detail}>
                         <label>No. of projects</label>
                         <input type='number' {...register("NoOfProjects", { required: "No. of projects is required" })} defaultValue={0} />
-                        {errors.NoOfProjects && <p>{errors.NoOfProjects.message}</p>}
                     </div>
+                        {errors.NoOfProjects && <p>{errors.NoOfProjects.message}</p>}
                     <div className={form.detail}>
                         <label>Year of Experience</label>
                         <input type='number' {...register("YearOfExperience", { required: "Year of Experience is required" })} defaultValue={0} />
-                        {errors.YearOfExperience && <p>{errors.YearOfExperience.message}</p>}
                     </div>
+                        {errors.YearOfExperience && <p>{errors.YearOfExperience.message}</p>}
                     <div className={form.detail}>
                         <label>Phone Number</label>
                         <input type='number' {...register("PhoneNumber", { required: "Phone Number is required", maxLength: { value: 10, message: "Phone No. should be 10 digits" }, minLength: { value: 10, message: "Phone No. should be 10 digits" } })} placeholder='Enter Phone Number' />
-                        {errors.PhoneNumber && <p>{errors.PhoneNumber.message}</p>}
                     </div>
+                        {errors.PhoneNumber && <p>{errors.PhoneNumber.message}</p>}
                     <div className={form.detail}>
                         <label>Image of Architect</label>
-                        <input type="file" className={form.file}{...register("ImageOfArchitect", { required: "Image of Architect is required" })} placeholder="Enter Architect Name" />
-                        {errors.ImageOfArchitect && <p>{errors.ImageOfArchitect.message}</p>}
+                        <input type="file" className={form.file}{...register("ImageOfArchitect", { required: "Image of Architect is required" })}/>
                     </div>
+                        {errors.ImageOfArchitect && <p>{errors.ImageOfArchitect.message}</p>}
                     <input type="submit" className={form.ArchiSubmit}/>
                 </form>
             </div>
--- Client/src/Client/ClientDetail.jsx
+++ Client/src/Client/ClientDetail.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
-
+import { useForm } from 'react-hook-form'
+import form from "../Detailform.module.css"
+import formimage from "../../Assets/ClientFormImage.png"
 function ClientDetail() {
-  return (
-    <div>ClientDetail</div>
-  )
+    const { register, handleSubmit, formState: { errors } } = useForm()
+
+    const onSubmit = (data) => {
+        console.log(data)
+    }
+    return (
+        <div className={form.Clientpage}>
+            <div className={form.Clientbox}>
+                <h1>Client</h1>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className={form.detail}>
+                        <label>Name</label>
+                        <input type='text' {...register("ClientName", { required: "Client Name is required", pattern: { value: /^[a-zA-Z]+$/, message: "Only Alphabets are allowed" } })} placeholder="Enter Client Name" />
+                    </div>
+                        {errors.ClientName && <p>{errors.ClientName.message}</p>}
+                    <div className={form.detail}>
+                        <label>Birth Year</label>
+                        <input type="number" {...register("BirthYear", { required: "Birth Year is required" , min:{value: 1900, message: "Year should be greater than 1900"} , max:{value: 2022, message: "Year should be less than 2022"}})} placeholder='Enter Birth Year' />  {/*Year should update Every years*/}
+                    </div>
+                        {errors.BirthYear && <p>{errors.BirthYear.message}</p>}
+                    <div className={form.detail}>
+                        <label>Phone Number</label>
+                        <input type='number' {...register("PhoneNumber", { required: "Phone Number is required", maxLength: { value: 10, message: "Phone No. should be 10 digits" }, minLength: { value: 10, message: "Phone No. should be 10 digits" } })} placeholder='Enter Phone Number' />
+                    </div>
+                        {errors.PhoneNumber && <p>{errors.PhoneNumber.message}</p>}
+                    <div className={form.detail}>
+                        <label>Image of Client</label>
+                        <input type="file" className={form.file}{...register("ImageOfClient", { required: "Image of Client is required" })} />
+                    </div>
+                        {errors.ImageOfClient && <p>{errors.ImageOfClient.message}</p>}
+                    <input type="submit" className={form.ClientSubmit}/>
+                </form>
+            </div>
+          <img src={formimage} alt="" className={form.image} />
+        </div>
+    )
 }
 
 export default ClientDetail
