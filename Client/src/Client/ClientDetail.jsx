471db22289b3af1294e1452d4ec2420d18f7c1fc
aniketgoyal51 <aniketgoyal791@gmail.com>
2024-03-01 12:16:39 +0530

    Created the landing page and did some correction in connection of pages

--- Client/src/Client/ClientDetail.jsx
+++ Client/src/Client/ClientDetail.jsx
@@ -22,22 +22,22 @@ function ClientDetail() {
                         <label>Name</label>
                         <input type='text' {...register("ClientName", { required: "Client Name is required", pattern: { value: /^[a-zA-Z]+$/, message: "Only Alphabets are allowed" } })} placeholder="Enter Client Name" />
                     </div>
-                        {errors.ClientName && <p>{errors.ClientName.message}</p>}
+                        {errors.ClientName && <p className={form.alert}>{errors.ClientName.message}</p>}
                     <div className={form.detail}>
                         <label>Birth Year</label>
                         <input type="number" {...register("BirthYear", { required: "Birth Year is required" , min:{value: 1900, message: "Year should be greater than 1900"} , max:{value: 2022, message: "Year should be less than 2022"}})} placeholder='Enter Birth Year' />  {/*Year should update Every years*/}
                     </div>
-                        {errors.BirthYear && <p>{errors.BirthYear.message}</p>}
+                        {errors.BirthYear && <p className={form.alert}>{errors.BirthYear.message}</p>}
                     <div className={form.detail}>
                         <label>Phone Number</label>
                         <input type='number' {...register("PhoneNumber", { required: "Phone Number is required", maxLength: { value: 10, message: "Phone No. should be 10 digits" }, minLength: { value: 10, message: "Phone No. should be 10 digits" } })} placeholder='Enter Phone Number' />
                     </div>
-                        {errors.PhoneNumber && <p>{errors.PhoneNumber.message}</p>}
+                        {errors.PhoneNumber && <p className={form.alert}>{errors.PhoneNumber.message}</p>}
                     <div className={form.detail}>
                         <label>Image of Client</label>
                         <input type="input" className={form.file}{...register("ImageOfClient", { required: "Image of Client is required" })} />
                     </div>
-                        {errors.ImageOfClient && <p>{errors.ImageOfClient.message}</p>}
+                        {errors.ImageOfClient && <p className={form.alert}>{errors.ImageOfClient.message}</p>}
                     <input type="submit" className={form.ClientSubmit}/>
                 </form>
             </div>
---
+++ Client/src/Common/Homepage.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import css from "../css/homepage.module.css"
+import logo from "../../Assets/Logo.png"
+import img from "../../Assets/HomeImage.png"
+import SignUp from './SignUp'
+
+function Homepage() {
+  return (
+    <div className={css.container}>
+        <div className={css.left}>
+            <img src={logo} alt="" className={css.logo}/>
+            <img src={img} alt="" className={css.img}/>
+        </div>
+        <div className={css.left}>
+            <h1 className={css.intro}>Introduction</h1>
+            <p className={css.pera}>Welcome to Build Vision, your all-in-one solution for architecture enthusiasts! Whether you're an aspiring architect, a professional designer, or someone who simply appreciates beautiful buildings, our app has something for you. <br />
+Explore a wide range of architectural marvels from around the world, discover the latest trends in design, and connect with like-minded individuals in our vibrant community. From iconic landmarks to cutting-edge innovations, immerse yourself in the fascinating world of architecture like never before. <br />
+Join us on this journey as we celebrate the creativity, ingenuity, and passion behind the structures that shape our world. Let's build something extraordinary together. Welcome to Build vision.</p>
+            <Link to="/SignUp"><button className={css.LogInBtn}>Log In</button></Link>
+        </div>
+    </div>
+  )
+}
+
+export default Homepage
