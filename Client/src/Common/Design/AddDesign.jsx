e5081c7b3c5a48494b106a906ec15445bed69218
aniketgoyal51 <aniketgoyal791@gmail.com>
2024-03-21 16:12:53 +0530

    Created update and delete of design which we have added in lst video or pr and created Ui for add design form and for displaying it

--- Client/src/Common/Design/AddDesign.jsx
+++ Client/src/Common/Design/AddDesign.jsx
@@ -4,13 +4,14 @@ import navcss from '../../css/Navigation.module.css'
 import css from '../../css/AddDesign.module.css'
 import DesignImage from '../../../Assets/DesignFormImage.png'
 import { useForm } from 'react-hook-form'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import axios from 'axios'
 
 function AddDesign() {
   const { id } = useParams()
   const { role } = useParams()
   const { register, handleSubmit, formState: { errors } } = useForm()
+  const navigate=useNavigate()
 
   const submit = (data) => {
     console.log(data)
@@ -28,6 +29,9 @@ function AddDesign() {
         .catch((err) => console.log(err))
     }
     ddata()
+
+    navigate(`/Profile/${role}/${id}`)
+    window.location.reload()
   }
 
 
@@ -36,33 +40,36 @@ function AddDesign() {
       <NavigationBar />
       <div className={css.body}>
         <div className={css.main}>
-          <img src="{DesignImage}" alt="" />
           <div className={css.container}>
-            <h1>Add Design</h1>
+            <h1 className={css.heading}>Add Design</h1>
             <form className={css.form}>
               <div className={css.detail}>
-                <label>Area of Plot</label>
-                <input type='text' {...register("AreaOfPlot", { required: "Area of Plot is required" })} placeholder="Enter Area of Plot" />
+                <label>Dimentions of Plot</label>
+                <input type='text' {...register("AreaOfPlot", { required: "Area of Plot is required" })} placeholder="Enter Area of Plot" className={css.input}/>
               </div>
               {errors.AreaOfPlot && <p className={css.alert}>{errors.AreaOfPlot.message}</p>}
               <div className={css.detail}>
-                <label>Area of Map</label>
-                <input type='text' {...register("AreaOfMap", { required: "Area of Map is required" })} placeholder="Enter Area of Map" />
+                <label>Dimentions of Map</label>
+                <input type='text' {...register("AreaOfMap", { required: "Area of Map is required" })} placeholder="Enter Area of Map" className={css.input}/>
               </div>
               {errors.AreaOfMap && <p className={css.alert}>{errors.AreaOfMap.message}</p>}
               <div className={css.detail}>
                 <label>Details of Map</label>
-                <input type='text' {...register("DetailsOfMap", { required: "Details of Map is required" })} placeholder="Enter Details of Map" />
+                <input type='text' {...register("DetailsOfMap", { required: "Details of Map is required" })} placeholder="Enter Details of Map" className={css.input}/>
               </div>
               {errors.DetailsOfMap && <p className={css.alert}>{errors.DetailsOfMap.message}</p>}
               <div className={css.detail}>
                 <label>Image of Map</label>
-                <input type='file' {...register("ImageOfDesign", { required: "Image of Design is required" })} placeholder="Upload Image of Map" />
+                <input type='file' {...register("ImageOfDesign", { required: "Image of Design is required" })} placeholder="Upload Image of Map" className={css.input}/>
               </div>
               {errors.ImageOfDesign && <p className={css.alert}>{errors.ImageOfDesign.message}</p>}
-              <button onClick={handleSubmit(submit)}>Submit</button>
+              <div className={css.btns}>
+              <button onClick={() => navigate(`/Profile/${role}/${id}`)} className={css.btn}>Cancel</button>
+              <button onClick={handleSubmit(submit)} className={css.btn}>Submit</button>
+              </div>
             </form>
           </div>
+          <img src={DesignImage} alt="" className={css.img}/>
         </div>
       </div>
     </div>
