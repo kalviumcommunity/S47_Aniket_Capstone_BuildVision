d0a6bc8cb541c246f07f222ee2e0f9713b0592dc
unknown <aniketgoyal791@gmail.com>
2024-11-08 14:11:48 +0530

    created error handling for backend

--- server/index.js
+++ server/index.js
@@ -27,60 +27,87 @@ mongoose.connect(process.env.CLUSTER, { dbName: "BuildVision" }, {
 })
 
 app.get('/ArchiSignU', Validation, async (req, res) => {
-    await archidetailschema.find({})
-        .then(result => res.json(result))
-        .catch(err => res.json(err))
+    try {
+        await archidetailschema.find({})
+            .then(result => res.json(result))
+            .catch(err => res.json(err))
+    } catch (error) {
+        console.log(Error)
+    }
 })
 app.get('/Profilefind/:role/:email', async (req, res) => {
-    if (req.params.role == "Architect") {
-        await archidetailschema.findOne({ ArchiEmail: req.params.email })
-            .then(result => res.json(result))
-            .catch(err => console.log(err))
-    }
-    else if (req.params.role == "Client") {
-        await clientdetailschema.findOne({ ClientEmail: req.params.email })
-            .then(result => { res.json(result) })
-            .catch(err => console.log(err))
+    try {
+        if (req.params.role == "Architect") {
+            await archidetailschema.findOne({ ArchiEmail: req.params.email })
+                .then(result => res.json(result))
+                .catch(err => console.log(err))
+        }
+        else if (req.params.role == "Client") {
+            await clientdetailschema.findOne({ ClientEmail: req.params.email })
+                .then(result => { res.json(result) })
+                .catch(err => console.log(err))
+        }
+    } catch (error) {
+        console.log(error)
     }
 })
 app.get('/ClientSignU', Validation, async (req, res) => {
-    await clientdetailschema.find({})
-        .then(result => res.json(result))
-        .catch(err => res.json(err))
+    try {
+        await clientdetailschema.find({})
+            .then(result => res.json(result))
+            .catch(err => res.json(err))
+    } catch (error) {
+        console.log(error)
+    }
 })
 app.get('/Profile/:role/:id', async (req, res) => {
-    id = req.params.id
-    if (req.params.role == "Architect") {
-        await archidetailschema.findById(id)
-            .then(result => {
-                res.json(result)
-            })
-            .catch(err => console.log(err))
-    }
-    else if (req.params.role == "Client") {
-        await clientdetailschema.findById(id)
-            .then(result => res.json(result))
-            .catch(err => console.log(err))
+    try {
+        id = req.params.id
+        if (req.params.role == "Architect") {
+            await archidetailschema.findById(id)
+                .then(result => {
+                    res.json(result)
+                })
+                .catch(err => console.log(err))
+        }
+        else if (req.params.role == "Client") {
+            await clientdetailschema.findById(id)
+                .then(result => res.json(result))
+                .catch(err => console.log(err))
+        }
+    } catch (error) {
+        console.log(error)
     }
 })
 app.get('/Designs', Validation, async (req, res) => {
-
-    await designdetailschema.find({})
-        .then(result => res.json(result))
-        .catch(err => res.json(err))
+    try {
+        await designdetailschema.find({})
+            .then(result => res.json(result))
+            .catch(err => res.json(err))
+    } catch (error) {
+        console.log(error)
+    }
 })
 app.get('/ShowDesign/:role/:id', async (req, res) => {
-    id = req.params.id
-    await designdetailschema.find({ ArchitectId: id })
-        .then(result => res.json(result))
-        .catch(err => res.json(err))
+    try {
+        id = req.params.id
+        await designdetailschema.find({ ArchitectId: id })
+            .then(result => res.json(result))
+            .catch(err => res.json(err))
+    } catch (error) {
+        console.log(error)
+    }
 })
 app.get('/Design/:role/:id/:did', async (req, res) => {
-    id = req.params.did
-    if (req.params.role == "Architect") {
-        await designdetailschema.findById(id)
-            .then(result => { res.json(result) })
-            .catch(err => console.log(err))
+    try {
+        id = req.params.did
+        if (req.params.role == "Architect") {
+            await designdetailschema.findById(id)
+                .then(result => { res.json(result) })
+                .catch(err => console.log(err))
+        }
+    } catch (error) {
+        console.log(error)
     }
 })
 app.get('/ClientForgetPass', async (req, res) => {
@@ -130,24 +157,32 @@ app.get('/ArchitectForgetPass', async (req, res) => {
 
 
 app.put('/Profileedit/:role/:id', async (req, res) => {
-    const id = req.params.id
-    if (req.params.role == "Architect") {
-        await archidetailschema.findByIdAndUpdate(id, req.body)
-            .then(result => { res.json(result) })
-            .catch(err => console.log(err))
-    }
-    else {
-        await clientdetailschema.findByIdAndUpdate(id, req.body)
-            .then(result => res.json(result))
-            .catch(err => console.log(err))
+    try {
+        const id = req.params.id
+        if (req.params.role == "Architect") {
+            await archidetailschema.findByIdAndUpdate(id, req.body)
+                .then(result => { res.json(result) })
+                .catch(err => console.log(err))
+        }
+        else {
+            await clientdetailschema.findByIdAndUpdate(id, req.body)
+                .then(result => res.json(result))
+                .catch(err => console.log(err))
+        }
+    } catch (error) {
+        console.log(error)
     }
 })
 app.put('/EditDesign/:role/:id/:did', async (req, res) => {
-    const id = req.params.did
-    if (req.params.role == "Architect") {
-        await designdetailschema.findByIdAndUpdate(id, req.body)
-            .then(result => { res.json(result) })
-            .catch(err => console.log(err))
+    try {
+        const id = req.params.did
+        if (req.params.role == "Architect") {
+            await designdetailschema.findByIdAndUpdate(id, req.body)
+                .then(result => { res.json(result) })
+                .catch(err => console.log(err))
+        }
+    } catch (error) {
+        console.log(error)
     }
 })
 app.put('/Clientchangepassword', async (req, res) => {
@@ -235,77 +270,87 @@ app.post('/ArchiLogin', async (req, res) => {
     }
 })
 app.post("/ArchiSignUp", archiupload.single("ImageOfArchitect"), async (req, res) => {
-    const afiledata = req.body
-    const userexist = await archidetailschema.findOne({ ArchiEmail: afiledata.ArchiEmail })
-    if (userexist) {
-        return res.status(400).json({ message: "user already exists" })
-    }
-    else {
-        if (req.file) {
-            afiledata.ImageOfArchitect = req.file.filename
-        }
-        if (afiledata.ArchiPassword) {
-            const pass = req.body.ArchiPassword
-            const hash = await bcrypt.hash(pass, 10)
-            archidetailschema.create({ ...afiledata, ArchiPassword: hash })
-                .then(result => {
-                    const token = jwt.sign({ result }, `${process.env.SECRET_KEY}`, { expiresIn: "1d" })
-                    res.json({ "result": "Signup Successful", "token": token })
-                })
-                .catch(err => console.log(err))
+    try {
+        const afiledata = req.body
+        const userexist = await archidetailschema.findOne({ ArchiEmail: afiledata.ArchiEmail })
+        if (userexist) {
+            return res.status(400).json({ message: "user already exists" })
         }
         else {
-            archidetailschema.create({ ...afiledata })
-                .then(result => {
-                    res.json({ "result": "Signup Successful" })
-                })
-                .catch(err => console.log(err))
+            if (req.file) {
+                afiledata.ImageOfArchitect = req.file.filename
+            }
+            if (afiledata.ArchiPassword) {
+                const pass = req.body.ArchiPassword
+                const hash = await bcrypt.hash(pass, 10)
+                archidetailschema.create({ ...afiledata, ArchiPassword: hash })
+                    .then(result => {
+                        const token = jwt.sign({ result }, `${process.env.SECRET_KEY}`, { expiresIn: "1d" })
+                        res.json({ "result": "Signup Successful", "token": token })
+                    })
+                    .catch(err => console.log(err))
+            }
+            else {
+                archidetailschema.create({ ...afiledata })
+                    .then(result => {
+                        res.json({ "result": "Signup Successful" })
+                    })
+                    .catch(err => console.log(err))
+            }
         }
+    } catch (error) {
+        console.log(error)
     }
 })
 app.post("/ClientSignUp", clientupload.single("ImageOfClient"), async (req, res) => {
-    const cfiledata = req.body
-    // console.log(pass);
-
-
-    const userexist = await clientdetailschema.findOne({ ClientEmail: cfiledata.ClientEmail })
-    if (userexist) {
-        // console.log(userexist);
-        return res.status(400).json({ message: "user already exists" })
-    }
-    else {
-
-        if (req.file) {
-            cfiledata.ImageOfClient = req.file.filename
-        }
-        if (cfiledata.ClientPassword) {
-            const pass = req.body.ClientPassword
-            const hash = await bcrypt.hash(pass, 10)
-            clientdetailschema.create({ ...cfiledata, ClientPassword: hash })
-                .then(result => {
-                    const token = jwt.sign({ result }, `${process.env.SECRET_KEY}`, { expiresIn: "1d" })
-                    res.json({ "result": "Signup Successful", "token": token })
-                })
-                .catch(err => console.log(err))
+    try {
+        const cfiledata = req.body
+        // console.log(pass);
+        const userexist = await clientdetailschema.findOne({ ClientEmail: cfiledata.ClientEmail })
+        if (userexist) {
+            // console.log(userexist);
+            return res.status(400).json({ message: "user already exists" })
         }
         else {
-            clientdetailschema.create({ ...cfiledata })
-                .then(result => {
-                    res.json({ "result": "Signup Successful" })
-                })
-                .catch(err => console.log(err))
+
+            if (req.file) {
+                cfiledata.ImageOfClient = req.file.filename
+            }
+            if (cfiledata.ClientPassword) {
+                const pass = req.body.ClientPassword
+                const hash = await bcrypt.hash(pass, 10)
+                clientdetailschema.create({ ...cfiledata, ClientPassword: hash })
+                    .then(result => {
+                        const token = jwt.sign({ result }, `${process.env.SECRET_KEY}`, { expiresIn: "1d" })
+                        res.json({ "result": "Signup Successful", "token": token })
+                    })
+                    .catch(err => console.log(err))
+            }
+            else {
+                clientdetailschema.create({ ...cfiledata })
+                    .then(result => {
+                        res.json({ "result": "Signup Successful" })
+                    })
+                    .catch(err => console.log(err))
+            }
         }
+    } catch (error) {
+        console.log(error)
     }
 })
 app.post('/AddDesign/:role/:id', designupload.single("ImageOfDesign"), (req, res) => {
-    if (req.params.role === "Architect") {
-        const dfiledata = req.body;
-        if (req.file) {
-            dfiledata.ImageOfDesign = req.file.filename;
+    try {
+        if (req.params.role === "Architect") {
+            const dfiledata = req.body;
+            if (req.file) {
+                dfiledata.ImageOfDesign = req.file.filename;
+            }
+            designdetailschema.create(dfiledata)
+                .then(result => res.send(result))
+                .catch(err => console.log(err));
         }
-        designdetailschema.create(dfiledata)
-            .then(result => res.send(result))
-            .catch(err => console.log(err));
+    } catch (error) {
+        console.log(error)
     }
 });
 
@@ -315,11 +360,15 @@ app.post('/AddDesign/:role/:id', designupload.single("ImageOfDesign"), (req, res
 
 
 app.delete('/DeleteDesign/:role/:id/:did', async (req, res) => {
-    const id = req.params.did
-    if (req.params.role == "Architect") {
-        await designdetailschema.findByIdAndDelete(id)
-            .then(result => { res.json(result) })
-            .catch(err => console.log(err))
+    try {
+        const id = req.params.did
+        if (req.params.role == "Architect") {
+            await designdetailschema.findByIdAndDelete(id)
+                .then(result => { res.json(result) })
+                .catch(err => console.log(err))
+        }
+    } catch (error) {
+        console.log(error)
     }
 })
 
